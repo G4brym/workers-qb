98aebda87b47aa0fe50f995049fb24436506f290
Gabriel Massadas <gmassadas@cloudflare.com>
2023-02-07 11:21:20 +0000

    Fix Raw not being exported

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { QueryBuilder } from './Builder'
 import { D1QB } from './Databases'
 import { OrderTypes, FetchTypes } from './enums'
 import { Where, Insert, Update, Delete, SelectAll, SelectOne } from './interfaces'
+import { Raw } from './tools'
 
-export { QueryBuilder, D1QB, OrderTypes, FetchTypes, Where, Insert, Update, Delete, SelectAll, SelectOne }
+export { QueryBuilder, D1QB, OrderTypes, FetchTypes, Where, Insert, Update, Delete, SelectAll, SelectOne, Raw }
