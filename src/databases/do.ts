41bb93bd0632af92d887b16806c690b5435f20f0
Luís Duarte <lduarte@cloudflare.com>
2024-08-21 15:56:59 +0100

    fix: make do database conform to return types

--- src/databases/do.ts
+++ src/databases/do.ts
@@ -1,6 +1,7 @@
 import { QueryBuilder } from '../builder'
 import { SqlStorageCursor, type SqlStorage } from '@cloudflare/workers-types/experimental'
 import { Query } from '../tools'
+import { FetchTypes } from '../enums'
 
 // There's no wrapper because DO databases return the Cursor to be consumed
 export class DurableObjectDatabaseQB extends QueryBuilder<{}> {
@@ -25,9 +26,32 @@ export class DurableObjectDatabaseQB extends QueryBuilder<{}> {
     if (query.arguments) {
       let stmt = this.db.prepare(query.query)
       // @ts-expect-error Their types appear to be wrong here
-      return stmt(...query.arguments) as SqlStorageCursor
+      const result = stmt(...query.arguments) as SqlStorageCursor
+      //FIXME: not efficient but the iterator that SRS is weird and I can only get it with a object form this way
+      if (query.fetchType == FetchTypes.ONE) {
+        return {
+          results: Array.from(result)[0],
+        }
+      }
+
+      // by default return everything
+      return {
+        results: Array.from(result),
+      }
     }
 
-    return this.db.exec(query.query)
+    const cursor = this.db.exec(query.query)
+
+    if (query.fetchType == FetchTypes.ONE) {
+      //FIXME: not efficient but the iterator that SRS is weird and I can only get it with a object form this way
+      return {
+        results: Array.from(cursor)[0],
+      }
+    }
+
+    // by default return everything
+    return {
+      results: Array.from(cursor),
+    }
   }
 }
