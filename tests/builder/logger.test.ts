1f0c1f85d52e9c59eda4ba9ce498a8f89bd30d27
Gabriel Massadas <gmassadas@cloudflare.com>
2024-11-09 15:33:13 +0000

    Migrate to vitest

--- tests/builder/logger.test.ts
+++ tests/builder/logger.test.ts
@@ -1,8 +1,9 @@
+import { describe, expect, it, vi } from 'vitest'
 import { QuerybuilderExceptionTest, QuerybuilderTest } from '../utils'
 
 describe('Logger', () => {
-  test('receive query in logger', async () => {
-    const loggerMock = jest.fn()
+  it('receive query in logger', async () => {
+    const loggerMock = vi.fn()
 
     await new QuerybuilderTest({
       logger: loggerMock,
@@ -18,16 +19,18 @@ describe('Logger', () => {
       .execute()
 
     expect(loggerMock.mock.calls).toHaveLength(1)
+    // @ts-ignore
     expect(loggerMock.mock.calls[0][0]).toMatchObject({
       query: 'SELECT id, name FROM testTable WHERE field = ?1',
       args: ['test'],
       fetchType: 'ALL',
     })
+    // @ts-ignore
     expect(loggerMock.mock.calls[0][1].duration >= 0).toBeTruthy()
   })
 
-  test('receive query in logger when there is an exception', async () => {
-    const loggerMock = jest.fn()
+  it('receive query in logger when there is an exception', async () => {
+    const loggerMock = vi.fn()
 
     const t = async () => {
       await new QuerybuilderExceptionTest({
@@ -47,11 +50,13 @@ describe('Logger', () => {
     await expect(t()).rejects.toThrow('Fake db error')
 
     expect(loggerMock.mock.calls).toHaveLength(1)
+    // @ts-ignore
     expect(loggerMock.mock.calls[0][0]).toMatchObject({
       query: 'SELECT id, name FROM testTable WHERE field = ?1',
       args: ['test'],
       fetchType: 'ALL',
     })
+    // @ts-ignore
     expect(loggerMock.mock.calls[0][1].duration >= 30).toBeTruthy()
   })
 })
