c67bd81c4678cd6b837dcbce8dba8376f6fd7f2c
Gabriel Massadas <5445926+G4brym@users.noreply.github.com>
2025-01-24 17:45:42 +0000

    Add support for unordered parameters in the update function (#104)
    Fixes #92

--- tests/unit/update.test.ts
+++ tests/unit/update.test.ts
@@ -1,5 +1,4 @@
 import { describe, expect, it } from 'vitest'
-import { ConflictTypes } from '../../src/enums'
 import { Raw } from '../../src/tools'
 import { QuerybuilderTest } from '../utils'
 
@@ -177,7 +176,7 @@ describe('Update Builder', () => {
         params: ['test', 345],
       },
       returning: ['id', 'field'],
-      onConflict: ConflictTypes.IGNORE,
+      onConflict: 'IGNORE',
     })
 
     expect(result.query).toEqual(
@@ -206,6 +205,204 @@ describe('Update Builder', () => {
       'UPDATE OR REPLACE testTable SET my_field = ?3, another = ?4 WHERE (field = ?1) AND (id = ?2) RETURNING id, field'
     )
     expect(result.arguments).toEqual(['test', 345, 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update one field with one where and verify arguments - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_data',
+      },
+      where: {
+        conditions: 'field = ?',
+        params: ['test_where'],
+      },
+    })
+
+    expect(result.query).toEqual('UPDATE testTable SET my_field = ?2 WHERE field = ?1')
+    expect(result.arguments).toEqual(['test_where', 'test_data'])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update with Raw sql values - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_data',
+        updated_at: new Raw('CURRENT_TIMESTAMP'),
+        another: '123',
+      },
+      where: {
+        conditions: 'field = ?',
+        params: ['test_where'],
+      },
+    })
+
+    expect(result.query).toEqual(
+      'UPDATE testTable SET my_field = ?2, updated_at = CURRENT_TIMESTAMP, another = ?3 WHERE field = ?1'
+    )
+    expect(result.arguments).toEqual(['test_where', 'test_data', '123'])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update one field with one where without returning - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+      },
+      where: {
+        conditions: 'field = ?',
+        params: ['test'],
+      },
+    })
+
+    expect(result.query).toEqual('UPDATE testTable SET my_field = ?2 WHERE field = ?1')
+    expect(result.arguments).toEqual(['test', 'test_update'])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update multiple field with one where without returning - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+      },
+      where: {
+        conditions: 'field = ?',
+        params: ['test'],
+      },
+    })
+
+    expect(result.query).toEqual('UPDATE testTable SET my_field = ?2, another = ?3 WHERE field = ?1')
+    expect(result.arguments).toEqual(['test', 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update multiple field with multiple where without returning - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+      },
+      where: {
+        conditions: ['field = ?', 'id = ?'],
+        params: ['test', 345],
+      },
+    })
+
+    expect(result.query).toEqual('UPDATE testTable SET my_field = ?3, another = ?4 WHERE (field = ?1) AND (id = ?2)')
+    expect(result.arguments).toEqual(['test', 345, 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update multiple field with multiple where with one returning - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+      },
+      where: {
+        conditions: ['field = ?', 'id = ?'],
+        params: ['test', 345],
+      },
+      returning: 'id',
+    })
+
+    expect(result.query).toEqual(
+      'UPDATE testTable SET my_field = ?3, another = ?4 WHERE (field = ?1) AND (id = ?2) RETURNING id'
+    )
+    expect(result.arguments).toEqual(['test', 345, 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update multiple field with multiple where with multiple returning - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+      },
+      where: {
+        conditions: ['field = ?', 'id = ?'],
+        params: ['test', 345],
+      },
+      returning: ['id', 'field'],
+    })
+
+    expect(result.query).toEqual(
+      'UPDATE testTable SET my_field = ?3, another = ?4 WHERE (field = ?1) AND (id = ?2) RETURNING id, field'
+    )
+    expect(result.arguments).toEqual(['test', 345, 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update on conflict ignore - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+      },
+      where: {
+        conditions: ['field = ?', 'id = ?'],
+        params: ['test', 345],
+      },
+      returning: ['id', 'field'],
+      onConflict: 'IGNORE',
+    })
+
+    expect(result.query).toEqual(
+      'UPDATE OR IGNORE testTable SET my_field = ?3, another = ?4 WHERE (field = ?1) AND (id = ?2) RETURNING id, field'
+    )
+    expect(result.arguments).toEqual(['test', 345, 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update on conflict replace - unnumbered parameters', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+      },
+      where: {
+        conditions: ['field = ?', 'id = ?'],
+        params: ['test', 345],
+      },
+      returning: ['id', 'field'],
+      onConflict: 'REPLACE',
+    })
+
+    expect(result.query).toEqual(
+      'UPDATE OR REPLACE testTable SET my_field = ?3, another = ?4 WHERE (field = ?1) AND (id = ?2) RETURNING id, field'
+    )
+    expect(result.arguments).toEqual(['test', 345, 'test_update', 123])
+    expect(result.fetchType).toEqual('ALL')
+  })
+
+  it('update multiple field with multiple where without returning - unnumbered parameters - complex case', async () => {
+    const result = new QuerybuilderTest().update({
+      tableName: 'testTable',
+      data: {
+        my_field: 'test_update',
+        another: 123,
+        third_field: 'third value',
+      },
+      where: {
+        conditions: ['field = ? AND another_field = ?', 'id = ?'],
+        params: ['test', 'another_test', 345],
+      },
+    })
+
+    expect(result.query).toEqual(
+      'UPDATE testTable SET my_field = ?4, another = ?5, third_field = ?6 WHERE (field = ?1 AND another_field = ?2) AND (id = ?3)'
+    )
+    expect(result.arguments).toEqual(['test', 'another_test', 345, 'test_update', 123, 'third value'])
     expect(result.fetchType).toEqual('ALL')
   })
 })
